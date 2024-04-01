6bc3fbfc3d4d624b5aae13bf3f73bf41435affcc
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-04-01 22:42:10 +0300

    Video Part 2

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
 
+// --- Connect to Database
+mongoose.connect(config.database);
+
+// --- On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database ' + config.database)  
+})
+
+// --- On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error: ' + config.database)  
+  })
+
+  
 const app = express();
 const users = require('./routes/users');
 
@@ -14,6 +29,10 @@ const port = 3000;
 
 // --- CORS Middleware
 app.use(cors());
+
+// --- Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // --- Body Parser Middleware
 app.use(bodyParser.json());
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,26 @@ router.get('/register', (req, res, next) => {
 
 });
 
+// -- Authenticate
+router.post('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+
+});
+
+
+// -- Profile
+// -- Register
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+
+});
+
+
+// -- Validate
+router.get('/validate', (req, res, next) => {
+    res.send('VALIDATE');
+
+});
+
+
 module.exports = router;
