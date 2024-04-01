f6beac50ada773debf142a672076f6a95f0e4311
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-04-01 21:49:25 +0300

    Video: 13:53

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-//const mongoose = require('mongoose');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
+//const router = express.Router();
+//const users = require('./routes/users');
+const users = require('/routes/users');
+
+
+// --- Port Number
 const port = 3000;
 
+// --- CORS Middleware
+app.use(cors());
 
-app.listen(port, () => {
-    console.log('Server started on port' + port);
+// --- Body Parser Middleware
+app.use(bodyParser.json());
+//app.use('/users', users);
+
+// --- Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
 });
+
+/// --- Start Server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
+
 
