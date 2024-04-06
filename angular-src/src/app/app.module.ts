ad0e9c8d42fa7a5586652d015f3fe04e3ad000cd
Timo Kivinen <Timo.Kivinen@student.lut.fi>
2024-04-06 17:31:58 +0300

    Video Part5 10:30

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
